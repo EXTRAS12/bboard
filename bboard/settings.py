49bfc5050763a17d7be210fb20a2ce0f23ba1b57
EXTRAS12 <extra-kent@mail.ru>
2023-10-09 13:26:30 +0300

    refactoring utiliries

--- bboard/settings.py
+++ bboard/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
--- main/utilities.py
+++ main/utilities.py
@@ -18,16 +18,16 @@ def send_new_comment_notification(comment):
     author = comment.bb.author
     context = {'author': author, 'host': host, 'comment': comment}
     subject = render_to_string('email/new_comment_letter_subject.txt', context)
-    body_text = render_to_string('email/settings.txt', context)
+    body_text = render_to_string('email/new_comment_letter_body.txt', context)
     author.email_user(subject, body_text)
 
 
 def send_activation_notification(user):
     if ALLOWED_HOSTS:
-        host = 'http://' + ALLOWED_HOSTS[0]
+        host = 'https://' + ALLOWED_HOSTS[0]
     else:
         host = 'http://localhost:8000'
-    context = {'user': user, 'host': host, 'sign': signer.sign(user.username)}
+    context = {'user': user, 'host': str(host), 'sign': str(signer.sign(user.username))}
     subject = render_to_string('email/activation_letter_subject.txt', context)
     body_text = render_to_string('email/activation_letter_body.html', context)
     user.email_user(subject, body_text)
